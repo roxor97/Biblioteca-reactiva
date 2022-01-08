d40570938e92688069816c2068668024bef6a423
Roxor <junyperez97@gmail.com>
2022-01-08 16:28:16 -0500

    first commit

---
+++ src/main/java/com/sofka/bibliotecaReactiva/BibliotecaReactivaApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.bibliotecaReactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/bibliotecaReactiva/BibliotecaReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.bibliotecaReactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
